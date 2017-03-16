a1b5ad70368cd3b186381ed2330bffc7aa1476f2
Weili An <China_Aisa@live.com>
2017-03-16 13:31:27 +0800

    Finish Control server: 	1. Request through GET, params provided by main server 	2. Add log output
    	Defect
    	1. Can't work when multiple server request for their access_token

--- Control_server/accesskey_acquirer.py
+++ Control_server/accesskey_acquirer.py
@@ -3,55 +3,72 @@ import time
 import os
 import sys
 import web
+import json
 """
 Using two server to provide wechat subscription service,
 one for acquring token, one for processing request
 """
 PATH = os.path.abspath(os.path.dirname(sys.argv[0])) # The path of this file
 accesskey_url = "https://api.weixin.qq.com/cgi-bin/token"
+web.config.debug = False # Set true to open debug output
 timer=os.times()
 urls=(
     "/access_token","token_acquirer"
 )
 class token_acquirer:
     def __init__(self):
-        self.token, self.expire_time= self.get_token()
-        print(self.token)
+        """
+        Log server's initialization
+        Store debug log
+        """
+        self.expire_time = 0
+        self.token = ""
+        msg = time.strftime("%Y-%m-%d %H:%M:%S")+"\tServer begins..."
+        print(msg)
+        with open(PATH+r"\..\Static\log","a") as log:
+            log.writelines(msg)
+            log.write("\n")
+            log.flush()
+            log.close()
     def GET(self):
-        if self.expire_time < 120:
-            self.token, self.expire_time = self.get_token()
+        # What about multi reuse??? 多个服务器请求时怎么办
+        credential = dict(web.input())
+        if self.expire_time < 120: # If access_token is not valid
+            self.token, self.expire_time = self.get_token(credential)
             return self.token
         else:
             return self.token
-    def get_token(self):
-        with open(PATH+r"\..\Static\log","w+") as log:
-            with open(PATH+r"\..\Static\accesskey_token.json") as credential_file: # Use relative path to find the credential file
-                while(1):
-                    credential = credential_file.readlines()
-                    credential = [i.strip() for i in credential]
-                    credential = eval("".join(credential)) # Catenate list to form json
-                    #print(credential)
-                    try:
-                        key = requests.get(accesskey_url,params=credential).json()
-                    except Exception as err:
-                        msg = time.strftime("%Y-%m-%d %H:%M:%S")+"\t[-]Error in get:"+err
-                        log.writelines(msg)
-                        log.flush()
-                        print(msg)
-                        continue
-                    if "errcode" in key:
-                        msg = time.strftime("%Y-%m-%d %H:%M:%S")+"\t[-]Unable to acquire access_token:"+key["errcode"]+"\t"+key["errmsg"]
-                        log.writelines(msg)
-                        log.flush()
-                        print(msg)
-                        time.sleep(10)
-                        continue
-                        # Send email?
-                    msg = time.strftime("%Y-%m-%d %H:%M:%S")+"\t[+]\ttoken:"+key["access_token"]
-                    log.writelines(msg)
-                    log.flush() # Save output
+    def get_token(self,credential):
+        with open(PATH+r"\..\Static\log","a") as log:
+            for i in range(5): # Try 5 times
+                try:
+                    key = requests.get(accesskey_url,params=credential).json()
+                except json.decoder.JSONDecodeError as err:
+                    msg = time.strftime("%Y-%m-%d %H:%M:%S")+"\t[-]Error in get:"+str(err)+"\t Usually inapporpriate GET input"
+                    # Write a function?
+                    log.writelines(msg+"\n")
+                    log.flush()
                     print(msg)
-                    return key["access_token"], key["expires_in"]
+                    #time.sleep(1)
+                    continue
+                if "errcode" in key:
+                    msg = time.strftime("%Y-%m-%d %H:%M:%S")+"\t[-]Unable to acquire access_token:"+str(key["errcode"])+"\t"+key["errmsg"]
+                    log.writelines(msg+"\n")
+                    log.flush()
+                    print(msg)
+                    #time.sleep(1)
+                    continue
+                    # Send email?
+                msg = time.strftime("%Y-%m-%d %H:%M:%S")+"\t[+]\ttoken:"+str(key["access_token"])
+                log.writelines(msg+"\n")
+                log.flush() # Save output
+                print(msg)
+                log.close()
+                return key["access_token"], key["expires_in"]
+            else:
+                print(msg)
+                log.close()
+                return "ERROR:"+msg,0 # Read the last 5 lines and print
 if __name__ == "__main__":
     app = web.application(urls,globals())
     app.run()
