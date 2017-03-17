41bfffcf076e43770875da45613fc9ecbe783d6d
Weili An <China_Aisa@live.com>
2017-03-17 08:15:34 +0800

    Developing sql database for control server, meeting problem: sqlite work in different thread

--- Control_server/accesskey_acquirer.py
+++ Control_server/accesskey_acquirer.py
@@ -4,25 +4,36 @@ import os
 import sys
 import web
 import json
+import sqlite3
+# 加密？-> 出租服务器
 """
 Using two server to provide wechat subscription service,
 one for acquring token, one for processing request
 """
 PATH = os.path.abspath(os.path.dirname(sys.argv[0])) # The path of this file
-accesskey_url = "https://api.weixin.qq.com/cgi-bin/token"
-web.config.debug = False # Set true to open debug output
 timer=os.times()
+
+web.config.debug = False # Set true to open debug output
+accesskey_url = "https://api.weixin.qq.com/cgi-bin/token"
 urls=(
-    "/access_token","token_acquirer"
+    "/access_token", "token_acquirer",
+    "/log", "log_replyier"
 )
+
+database = sqlite3.connect(PATH+r"\..\Static\appid.db") # Initializing database
+cursor = database.cursor()
+# Create table to store tokens
+try:
+    database.execute('''CREATE TABLE appid_token(appid text, current_token text, last_time real)''')
+except sqlite3.OperationalError:
+    pass
+
 class token_acquirer:
     def __init__(self):
         """
         Log server's initialization
         Store debug log
         """
-        self.expire_time = 0
-        self.token = ""
         msg = time.strftime("%Y-%m-%d %H:%M:%S")+"\tServer begins..."
         print(msg)
         with open(PATH+r"\..\Static\log","a") as log:
@@ -31,13 +42,31 @@ class token_acquirer:
             log.flush()
             log.close()
     def GET(self):
-        # What about multi reuse??? 多个服务器请求时怎么办
+        # What about multi reuse??? 多个服务器请求时怎么办 -> SQL
         credential = dict(web.input())
-        if self.expire_time < 120: # If access_token is not valid
-            self.token, self.expire_time = self.get_token(credential)
+        print(credential["appid"])
+        # Get elapse time to calculate expire_time -> time.time() store last record in sql, calculate with this request
+        # SELECT expire_time FROM appid_token WHERE appid =
+        # SQL hacking>
+        try:
+            self.last_time = cursor.execute("SELECT last_time FROM appid_token WHERE appid = ?",credential["appid"])
+        except: # New appid?
+            # INSERT INTO appid_token VALUES(appid, current_token, last_time)
+            self.last_time = 0
+            cursor.execute("INSERT INTO appid_token VALUES(?,?,?)",(credential["appid"],"",self.last_time))
+
+        # Simplify this if clause?
+        if time.time() - self.last_time > 7180: # If access_token is nearly invalid
+            self.token = self.get_token(credential)
+            if "ERROR" in self.token:
+                return self.token
+            # Update token and time
+            # UPDATE Person SET current_token = , expire_time =  WHERE appid =
+            cursor.execute("UPDATE appid_token SET current_token =? ,last_time =?  WHERE appid = ?",(self.token,time.time(),credential["appid"]))
             return self.token
         else:
-            return self.token
+            # find token in sql
+            return cursor.execute("SELECT current_token,last_time FROM appid_token WHERE appid=?",credential["appid"])
     def get_token(self,credential):
         with open(PATH+r"\..\Static\log","a") as log:
             for i in range(5): # Try 5 times
@@ -59,16 +88,24 @@ class token_acquirer:
                     #time.sleep(1)
                     continue
                     # Send email?
-                msg = time.strftime("%Y-%m-%d %H:%M:%S")+"\t[+]\ttoken:"+str(key["access_token"])
+                msg = time.strftime("%Y-%m-%d %H:%M:%S")+"\t[+]token:"+str(key["access_token"])
                 log.writelines(msg+"\n")
                 log.flush() # Save output
                 print(msg)
                 log.close()
-                return key["access_token"], key["expires_in"]
+                return key["access_token"]#, key["expires_in"]
             else:
                 print(msg)
                 log.close()
-                return "ERROR:"+msg,0 # Read the last 5 lines and print
+                return "ERROR:"+msg#,0 # Read the last 5 lines and print
+class log_replyier():
+    def GET(self):
+        try:
+            appid = dict(web.input())["appid"]
+        except Exception as err:
+            return str(err)
+    def log_finder(self,appid):
+        pass
 if __name__ == "__main__":
     app = web.application(urls,globals())
     app.run()
