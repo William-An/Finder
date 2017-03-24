bf6bcb225731eca25e0bbbd452a724a528d47b77
Weili An <China_Aisa@live.com>
2017-03-24 17:04:43 +0800

    Finish customize_userManual.py, adding commandline options

--- wechat_config/customize_userManual.py
+++ wechat_config/customize_userManual.py
@@ -1,13 +1,19 @@
 import requests
 import os
 import sys # provide option
+import argparse # parse options
+
 PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
 token_url = "https://api.weixin.qq.com/cgi-bin/token" # Change to control server
 create_interface = "https://api.weixin.qq.com/cgi-bin/menu/create"
+get_Allinterface = "https://api.weixin.qq.com/cgi-bin/menu/get"
+get_Currentinterface = "https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info"
+del_interface = "https://api.weixin.qq.com/cgi-bin/menu/delete"
+
+# del, getall, getcurrent could be merge into one method by changing params
 
 class userInterface:
-    token = ""
-    key = dict()
+    credential = dict()
     @staticmethod
     def init():
         # Initialization: acquire access_token from control server
@@ -16,35 +22,60 @@ class userInterface:
             # print(credential)
             credential_file.close()
         try:
-            userInterface.key = requests.get(url=token_url,params=credential).json()
-            userInterface.token = userInterface.key["access_token"]
+            key = requests.get(url=token_url,params=credential).json()
+            token = key["access_token"]
+            userInterface.credential.update([("access_token",token)])
         except Exception as err:
-            if "errcode" in userInterface.key:
-                print("ERROR: errcode:%\t%",(userInterface.key["errcode"],userInterface.key["errmsg"]))
+            if "errcode" in key:
+                print("ERROR: errcode:%\t%",(key["errcode"],key["errmsg"]))
             else:
                 print("ERROR: "+str(err))
             exit()
     @staticmethod
-    def createManual():
-        with open(PATH+r"\..\Static\userInterface.json","rb") as config_file:
+    def createManual(file_addr):
+        with open(file_addr,"rb") as config_file:
             try:
                 # print([ i.decode() for i in config_file.readlines()])
                 config = eval("".join([i.strip().decode() for i in config_file.readlines()]))
             except Exception as err:
                 print(str(err))
                 exit()
-            credential = {'access_token':userInterface.token}
-            response = requests.post(create_interface,params=credential,data=config).json()
+            response = requests.post(create_interface,params=userInterface.credential,data=config).json()
             print("Result\nerrcode:",response["errcode"],response["errmsg"])
-    """
     @staticmethod
     def delInterface():
+        # Write into function? paras are url and credential?
+        response = requests.get(del_interface,params=userInterface.credential).json()
+        print("Result\nerrcode:",response["errcode"],response["errmsg"])
     @staticmethod
-    def viewInterface():
-    """
+    def viewAllInterface():
+        response = requests.get(get_Allinterface,params=userInterface.credential).json()
+        if "errcode" in response:
+            print("Result\nerrcode:",response["errcode"],response["errmsg"])
+        else:
+            print(response)
+    @staticmethod
+    def viewCurrentInterface():
+        response = requests.get(get_Currentinterface,params=userInterface.credential).json()
+        if "errcode" in response:
+            print("Result\nerrcode:",response["errcode"],response["errmsg"])
+        else:
+            print(response)
+
 if __name__ == "__main__":
-    # Process different function through options
+    parser = argparse.ArgumentParser(description="Provide access to modify/view wechat customized usermanual")
+    parser.add_argument('-d','--delete',action='store_true',help='Delete ALL interfaces')
+    parser.add_argument('-l','--list',action="store_true",help="List all userinterfaces")
+    parser.add_argument('-i','--inspect',action="store_true",help="List current userinterface")
+    parser.add_argument('-c','--config',help="upload the userinterface configuration")
+    option=parser.parse_args()
     userInterface.init()
-    userInterface.createManual()
-    # Some methods
+    if option.delete:
+        userInterface.delInterface()
+    elif option.list:
+        userInterface.viewAllInterface()
+    elif option.inspect:
+        userInterface.viewCurrentInterface()
+    else:
+        userInterface.createManual(option.config)
     exit()
